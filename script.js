b29f1264d1ad9184768d1bd094aaa0e3c2e6dc39
AmethystCrystal <140659819+AmethystCrystal@users.noreply.github.com>
2023-10-27 20:17:54 -0400

    created a second event listener for a second button

--- script.js
+++ script.js
@@ -6,15 +6,26 @@ flexContainer.appendChild(p);
 
 const flexItems = document.querySelectorAll(".container .flex-item");
 
-flexItems.forEach(function(item){
+flexItems.forEach(function (item) {
     item.textContent = "Nullam faucibus lectus a lectus posuere, vitae mollis lorem fringilla. Phasellus ultricies ut nulla vitae mattis. Nulla finibus eleifend euismod. Cras pellentesque ex a rhoncus suscipit. Nulla accumsan sem fringilla augue consequat, id pulvinar sapien eleifend. Nulla augue libero, luctus sit amet nibh nec, faucibus tincidunt diam. Suspendisse id euismod libero. Sed semper auctor dolor eu gravida. Praesent euismod magna tempus mauris gravida dictum. Vestibulum vehicula aliquet nisl, quis viverra odio fermentum vel. Integer vulputate libero ut purus iaculis finibus. Cras rhoncus erat ut ipsum hendrerit tempor. Aenean finibus euismod eros sed cursus. Ut vulputate leo id maximus consequat. Duis ac posuere neque, eu ultrices quam.";
-    
+
 })
 
 const searchButton = document.querySelector(".btn-outline-success");
 
-
 function button(event) {
-  searchButton.textContent = "Searching!";
+    searchButton.textContent = "Searching!";
 }
 searchButton.addEventListener("click", button);
+
+const question = document.querySelector("#faq")
+
+function FAQ(event) {
+    
+    const confirmed = confirm("Are you ready to get all your questions answered?")
+
+    if (!confirmed) {
+        event.preventDefault();
+    }
+
+} question.addEventListener("click", FAQ);
