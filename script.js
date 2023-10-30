543328fb8c0343a6cfbf6990e488827ec7d5cdb5
AmethystCrystal <140659819+AmethystCrystal@users.noreply.github.com>
2023-10-29 21:21:48 -0400

    navigated between elements

--- script.js
+++ script.js
@@ -9,21 +9,21 @@ const flexItems = document.querySelectorAll(".container .flex-item");
 
 flexItems.forEach(function (item) {
     item.textContent = "Nullam faucibus lectus a lectus posuere, vitae mollis lorem fringilla. Phasellus ultricies ut nulla vitae mattis. Nulla finibus eleifend euismod. Cras pellentesque ex a rhoncus suscipit. Nulla accumsan sem fringilla augue consequat, id pulvinar sapien eleifend. Nulla augue libero, luctus sit amet nibh nec, faucibus tincidunt diam. Suspendisse id euismod libero. Sed semper auctor dolor eu gravida. Praesent euismod magna tempus mauris gravida dictum. Vestibulum vehicula aliquet nisl, quis viverra odio fermentum vel. Integer vulputate libero ut purus iaculis finibus. Cras rhoncus erat ut ipsum hendrerit tempor. Aenean finibus euismod eros sed cursus. Ut vulputate leo id maximus consequat. Duis ac posuere neque, eu ultrices quam.";
-
 })
+
 
 const searchButton = document.querySelector(".btn-outline-success");
 
 function button(event) {
-searchButton.textContent = "Searching!";
-window.open("https://google.com");
-}searchButton.addEventListener("click", button);
-searchButton.addEventListener("mouseover", function (){
+    searchButton.textContent = "Searching!";
+    window.open("https://google.com");
+} searchButton.addEventListener("click", button);
+searchButton.addEventListener("mouseover", function () {
     searchButton.style.backgroundColor = "red";
 
-searchButton.addEventListener("mouseout", function (){
-    searchButton.style.backgroundColor = "";
-})
+    searchButton.addEventListener("mouseout", function () {
+        searchButton.style.backgroundColor = "";
+    })
 })
 
 
@@ -31,12 +31,12 @@ const question = document.querySelector("#faq")
 
 
 function FAQ(event) {
-    
-const confirmed = window.confirm("Are you ready to get all your questions answered?")
 
-if (!confirmed) {
-    event.preventDefault();
- }
+    const confirmed = window.confirm("Are you ready to get all your questions answered?")
+
+    if (!confirmed) {
+        event.preventDefault();
+    }
 } question.addEventListener("click", FAQ);
 
 
@@ -51,13 +51,27 @@ const email = document.querySelector("#userName");
 const message = document.querySelector("#validationMessage");
 const submitButton = document.querySelector("#submit");
 
-email.addEventListener("input", function (){
-const minLength = 15;
-const value = email.value;
+email.addEventListener("input", function () {
+    const minLength = 15;
+    const value = email.value;
 
-if (value.length < minLength) {
-    validationMessage.textContent = `Email must be at least ${minLength} characters long.`;
-} else {
-    validationMessage.textContent = "";
-}
+    if (value.length < minLength) {
+        validationMessage.textContent = `Email must be at least ${minLength} characters long.`;
+    } else {
+        validationMessage.textContent = "";
+    }
 });
+
+function navigate() {
+
+    var startElement = document.getElementById('startItem');
+    var targetElement = startElement.previousElementSibling;
+    targetElement.style.color = 'red';
+}
+
+
+document.querySelector("#heading1").innerHTML = document.querySelector("#heading2").firstChild.nodeValue;
+document.querySelector("#heading3").innerHTML = document.querySelector("#heading2").firstChild.nodeValue;
+
+
+
